f1258594b3898d08d3eda9f7b57a33c6b708005a
Kut Zhang <kut@sanlea.cn>
2022-09-02 11:31:42 +0800

    fix

--- settings/kubernetes/16/statefulset.groovy
+++ settings/kubernetes/16/statefulset.groovy
@@ -1,6 +1,9 @@
-env.APP_NAME="k8s-16.statefulset"
+env.APP_NAME="k8s-16-statefulset"
+env.IMAGE_NAME="k8s-16.statefulset"
+
 env.GIT_URL="https://github.com/devops-onboard-sources/k8s-16-statefulset.git"
 env.GIT_BRANCH="main"
+
 env.K8S_NAMESPACE="study"
 env.K8S_REPLICAS="3"
 env.K8S_REQUEST_CPU="100m"
@@ -10,4 +13,3 @@ env.K8S_LIMIT_MEMORY="600Mi"
 env.K8S_STORAGE_CLASS_NAME="sanlea-k8s-worker-2"
 env.K8S_STORAGE_SIZE="10Mi"
 env.K8S_SERVICE_NAME="k8s-16-statefulset"
-env.DEPLOY_TYPE="statefulset"
