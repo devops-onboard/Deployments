9e7dee1bb37a0b8d2348bf7e78121e893b77a5ed
Kut Zhang <kut@sanlea.cn>
2022-09-02 12:14:13 +0800

    fix

--- settings/kubernetes/16/statefulset.groovy
+++ settings/kubernetes/16/statefulset.groovy
@@ -1,4 +1,4 @@
-env.APP_NAME="k8s-16.statefulset"
+env.APP_NAME="k8s-16-statefulset"
 env.IMAGE_NAME="k8s-16.statefulset"
 
 env.GIT_URL="https://github.com/devops-onboard-sources/k8s-16-statefulset.git"
